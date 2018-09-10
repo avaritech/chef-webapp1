31837827c7397f68fe5202f9fb71e6e5fc99dccb
root <root@ip-172-31-31-212.us-east-2.compute.internal>
2018-09-10 20:41:52 +0000

    updated README.md

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,28 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+
+apt_update 'update the apt cache daily' do
+        frequency 86_400
+        action :periodic
+end
+
+
+package 'apache2'
+
+service 'apache2' do
+        supports status: true
+        action [:enable, :start]
+end
+
+
+file '/var/www/html/index.html' do
+        content '<html
+        <body>
+        <h1>web server 1</h1>
+        </body>
+        </html>'
+end
+
